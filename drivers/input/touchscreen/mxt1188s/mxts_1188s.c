4886eb61e2b03f999b3a17b96f95d042677b6a78
sub77 <sub77@ymail.com>
2016-01-25 07:41:16 +0100

    matissewifi: fix build with disabled DVFS

--- drivers/input/touchscreen/mxt1188s/mxts_1188s.c
+++ drivers/input/touchscreen/mxt1188s/mxts_1188s.c
@@ -1054,11 +1054,11 @@ static void mxt_report_input_data(struct mxt_data *data)
 #endif
 			input_sync(data->input_dev);
 	}
-
+#ifdef DVFS_FUCKER
 #if TSP_BOOSTER
 	mxt_set_dvfs_lock(data, count);
 #endif
-
+#endif
 	data->finger_mask = 0;
 }
 
@@ -2285,10 +2285,11 @@ static int mxt_stop(struct mxt_data *data)
 #if ENABLE_TOUCH_KEY
 	mxt_release_all_keys(data);
 #endif
+#ifdef DVFS_FUCKER
 #if TSP_BOOSTER
 	mxt_set_dvfs_lock(data, -1);
 #endif
-
+#endif
 	data->mxt_enabled = false;
 
 	return 0;
@@ -2437,12 +2438,15 @@ static int mxt_touch_finish_init(struct mxt_data *data)
 		dev_err(&client->dev, "Failed to clear CHG pin\n");
 		goto err_req_irq;
 	}
-
+#ifdef DVFS_FUCKER
 #if TSP_BOOSTER
 	mxt_init_dvfs(data);
 #endif
+#endif
+#ifdef DVFS_FUCKER
 #if MXT_TKEY_BOOSTER
 	mxt_tkey_init_dvfs(data);
+#endif
 #endif
 
 	dev_info(&client->dev,  "Mxt touch controller initialized\n");
